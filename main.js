23ee67b6cef220b6a45d0a85d66024d5e6392631
SamarthCool <106153265+SamarthCool@users.noreply.github.com>
2023-04-28 15:43:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function setup()
+{
+    canvas=createCanvas(640, 420);
+    canvas.center();
+    objectdetector=ml5.objectDetector("cocossd" , modelLoaded);
+    document.getElementById("status").innerHTML="status:detecting object";
+}
+img="";
+status="";
+objects=[];
+function preload()
+{
+    img=loadImage("dog_cat.jpg");
+}
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+
+    if (status != " " )
+    {
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML="Status ' Detected Objects";
+    fill("red");
+    percent=floor(objects[i].confidence * 100);
+    text(objects[i].label + " " + percent + "%" ,objects[i].x + 15, objects[i].y + 15);
+    noFill();
+    stroke("red");
+    rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+function modelLoaded()
+{
+    console.log("Model has Loaded");
+    status=true;
+    objectdetector.detect(img, gotresult);
+}
+function gotresult(error, results)
+{
+    if (error){
+        console.log(error);
+    }
+    
+        console.log(results);
+    
+        objects=results;
+}
